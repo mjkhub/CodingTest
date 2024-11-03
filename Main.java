48bf88d653f3f893106cbfbcc8d2c1af13c7cbc4
jay <rlaakswo0687@gmail.com>
2024-11-03 14:48:54 +0900

    Boj 1032 done

--- Main.java
+++ Main.java
@@ -1,12 +1,34 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+    public static StringBuilder sb = new StringBuilder();
+    public static String[] arr;
+    public static int N ;
+    public static void main(String[] args) throws IOException { //boj 1032
 
+        N = Integer.parseInt(br.readLine());
+        arr = new String[N];
+        for(int i=0; i<N; i++) arr[i] = br.readLine();
 
+        int stringLength = arr[0].length();
+        for(int i =0; i<stringLength; i++){
+            char c = compareCharAtIndex(i);
+            sb.append(c);
+        }
+        System.out.println(sb);
+
+    }
+
+    public static char compareCharAtIndex(int index){
+        char c = arr[0].charAt(index);
+        for (int i=1; i<N; i++){
+            if( c != arr[i].charAt(index)) return '?';
+        }
+        return c;
     }
 }
