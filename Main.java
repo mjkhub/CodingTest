702b06556b9fa5d5c01408c1f2ce7733773394d7
jay <rlaakswo0687@gmail.com>
2024-11-06 15:36:43 +0900

    boj 1929 done

--- Main.java
+++ Main.java
@@ -1,55 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-public class Main { // boj 1051
+public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException { //boj 1141
+    public static void main(String[] args) throws IOException { // boj 1929
 
-        int N = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int m = Integer.parseInt(s[0]);
+        int n = Integer.parseInt(s[1]);
 
-        int answer = 0;
-
-        List<String> arr = new ArrayList<>();
-        for(int i =0; i<N; i++) {
-            String word = br.readLine();
-            if(!arr.contains(word)) arr.add(word);
+        StringBuilder sb = new StringBuilder();
+        for(int i=m; i<=n; i++){
+            if(isPrimeNumber(i)) sb.append(i).append('\n');
         }
-
-        for(int i=0; i<arr.size(); i++){
-            String a = arr.get(i);
-            boolean goodToAdd = true;
-            for(int j=0; j<arr.size(); j++){
-                if(i != j) {
-                    String b = arr.get(j);
-                    if(checkPrefix(a,b) ){
-                        goodToAdd = false;
-                        continue;
-                    }
-                }
-            }
-            if(goodToAdd) answer++;
-        }
-
-        System.out.println(answer);
-
+        System.out.println(sb);
     }
 
 
+    public static boolean isPrimeNumber(int n){
+        if( n == 1) return false;
 
-    public static boolean checkPrefix(String a, String b){ //a가 b의 prefix 인지 확인
-        if(a.length() > b.length()) return false;
-
-        boolean isPrefix = true;
-        for(int i=0; i<a.length(); i++){
-            if(a.charAt(i) != b.charAt(i)) isPrefix =false;
+        int sqrt = (int)Math.sqrt(n);
+        for(int i=2;i <=sqrt; i++){
+            if( n % i ==0) return false;
         }
-        return isPrefix;
+        return true;
     }
+
 }
