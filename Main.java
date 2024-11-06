c24291ec5f43e990a6ec6e7ba7b44a548389d24c
jay <rlaakswo0687@gmail.com>
2024-11-06 13:26:43 +0900

    Boj 1051

--- Main.java
+++ Main.java
@@ -1,34 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Main { // boj 1094
+public class Main { // boj 1051
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException { //boj 1357
+    public static void main(String[] args) throws IOException { //boj 2869
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        int[][] arr = new int[N][M];
 
-        int x = Integer.parseInt(br.readLine());
-        if( x == 64){
-            System.out.println(1);
-            return;
-        }
-
-        int shortBar = 64;
-        int barSum = 0;
-        int count = 0;
-
-        while(barSum < x){
-            shortBar = shortBar / 2;
-            if(barSum + shortBar > x){ // do nothing
-
-            }else{
-                barSum += shortBar;
-                count++;
+        for(int i=0; i<N; i++){
+            String[] line = br.readLine().split("");
+            for(int j=0; j<M; j++){
+                arr[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        System.out.println(count);
+        int minN_M = N < M ? N : M;
+        int answerLength = 1;
+
+
+        label: for(int k=2; k<=minN_M; k++){
+            for(int i=0; i<=N-k; i++){
+                for(int j=0; j<= M-k; j++){
+                    if(arr[i][j] == arr[i][j+k-1] &&
+                            arr[i][j+k-1] == arr[i+k-1][j+k-1] &&
+                            arr[i+k-1][j+k-1] == arr[i+k-1][j]){
+                        answerLength = k;
+                        continue label;
+                    }
+                }
+            }
+        }
+        System.out.println(answerLength*answerLength);
+
+
 
     }
 
