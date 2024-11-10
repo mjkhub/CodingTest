98c929719d6dc23455516e455ad9da5b2cbd46b8
jay <rlaakswo0687@gmail.com>
2024-11-10 12:46:40 +0900

    Boj 1991

--- Main.java
+++ Main.java
@@ -6,38 +6,62 @@ import java.util.*;
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static boolean[] visit;
-    public static int[] buf;
-    public static int N;
-    public static int M;
-    public static void main(String[] args) throws IOException { // boj 15649
 
-        String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
-        buf = new int[M];
-        visit = new boolean[N+1];
-        dfs(0);
+    public static int N;
+    public static char[][] binaryTree;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException { // boj 1991
+
+        N = Integer.parseInt(br.readLine());
+        binaryTree = new char[N][2];
+
+        for(int i=0; i<N; i++){ // init
+            String data = br.readLine().replace(" ", "");
+            char node = data.charAt(0);
+            char left = data.charAt(1);
+            char right = data.charAt(2);
+            binaryTree[node-'A'][0] = left;
+            binaryTree[node-'A'][1] = right;
+        }
+        preOrder(0);
+        sb.append("\n");
+        inOrder(0);
+        sb.append("\n");
+        postOrder(0);
+        System.out.println(sb);
+
     }
 
-    public static void dfs(int depth){
-
-        if(depth == M ){
-            StringBuilder sb = new StringBuilder();
-            for(int value : buf)
-                sb.append(value).append(' ');
-            System.out.println(sb);
-            return;
-        }// base case
-
-        for(int i=1; i<=N; i++){
-            if(!visit[i]){
-                visit[i] = true;
-                buf[depth] = i;
-                dfs(depth + 1);
-                visit[i] = false;
+    public static void preOrder(int n){
+        sb.append((char) ('A'+ n) ); //visit
+        char[] children = binaryTree[n];
+        for(char child : children){
+            if(child!='.'){
+                preOrder(child-'A');
             }
         }
+    }
+
+    public static void postOrder(int n){
+        char[] children = binaryTree[n];
+        for(char child : children){
+            if(child!='.'){
+                postOrder(child-'A');
+            }
+        }
+        sb.append((char) ('A'+ n) ); //visit
+    }
+
+
+    public static void inOrder(int n){
+        char[] children = binaryTree[n];
+        char left = children[0];
+        char right = children[1];
+        if(left != '.') inOrder(left-'A');
+
+        sb.append((char) ('A'+ n) ); //visit
+        if(right != '.') inOrder(right-'A');
+
     }
 
 
