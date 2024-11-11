bbb878bef862f027da76a53ded5b67938cbec953
jay <rlaakswo0687@gmail.com>
2024-11-11 10:49:38 +0900

    Boj 1551

--- Main.java
+++ Main.java
@@ -8,29 +8,29 @@ public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static int M;
 
-    public static boolean[] arr;
-
     public static void main(String[] args) throws IOException {
-        M = Integer.parseInt(br.readLine());
-        arr = new boolean[4];
-        arr[1] = true;
+        String[] s = br.readLine().split(" ");
+        int k = Integer.parseInt(s[1]);
 
-        for(int i=0; i<M; i++){
-            String[] s = br.readLine().split(" ");
-            int cup1 = Integer.parseInt(s[0]);
-            int cup2 = Integer.parseInt(s[1]);
-            swap(cup1, cup2);
+        int[] array =
+                Arrays.stream(br.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
+        for(int i=0; i<k; i++) genArray(array);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<array.length-k; i++){
+            sb.append(array[i]);
+            if(i!= array.length-k-1)
+                sb.append(',');
         }
-        for(int i=1; i<=3; i++){
-            if(arr[i]) System.out.println(i);
+        System.out.println(sb);
+    }
+
+    public static void genArray(int[] array){
+        for(int i=0; i<array.length-1; i++){
+            array[i] = array[i+1] - array[i];
         }
     }
-    public static void swap(int cup1, int cup2){
 
-        boolean temp = arr[cup1];
-        arr[cup1] = arr[cup2];
-        arr[cup2] = temp;
-    }
 
 
 
