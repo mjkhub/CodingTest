e5a13ea6c0d4a05f5c48b1a5264452009279bd48
jay <rlaakswo0687@gmail.com>
2024-11-14 21:44:39 +0900

    Boj 8320

--- Main.java
+++ Main.java
@@ -5,42 +5,26 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException { // boj 15953
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(br.readLine());
-        for(int i=0; i<t; i++){
-            String[] s = br.readLine().split(" ");
-            printMoney(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+
+    public static void main(String[] args) throws IOException { //boj 8320
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        for(int i=1; i*i<=n; i++){
+
+            for(int j=i*i; j<=n; j+=i){
+                count++;
+            }
         }
+        System.out.println(count);
+
+
+
+
     }
 
-    public static void printMoney(int first, int second){
-        int first1 = getFirst(first);
-        int second1 = getSecond(second);
-        System.out.println(first1 +second1);
-    }
-
-    public static int getFirst(int first){
-
-        if(first == 1) return 5000000;
-        else if(2<=first && first<=3)   return 3000000;
-        else if(4<=first && first<=6)   return 2000000;
-        else if(7<=first && first<=10)  return  500000;
-        else if(11<=first && first<=15) return  300000;
-        else if(16<=first && first<=21) return  100000;
-        else return 0;
-    }
-
-    public static int getSecond(int first){
-
-        if(first == 1) return 5120000;
-        else if(2<=first && first<=3)   return 2560000;
-        else if(4<=first && first<=7)   return 1280000;
-        else if(8<=first && first<=15)  return  640000;
-        else if(16<=first && first<=31) return  320000;
-        else return 0;
-    }
 
 
 
