d19d19346823df6c8ed43fc1fbeb19f706c32690
jay <rlaakswo0687@gmail.com>
2024-11-28 23:14:44 +0900

    BOJ 1920

--- Main.java
+++ Main.java
@@ -5,80 +5,36 @@ import java.util.*;
 
 public class Main {
 
-
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int n;
-    public static int m;
-    public static int[][] graph;
-    public static boolean[][] bfsVisited;
-    public static int[][] minDepth;
+    public static StringBuilder sb = new StringBuilder();
+    public static int N, M;
+    public static int[] arr, res;
+    public static void main(String[] args) throws IOException{
+        // TODO Auto-generated method stub
+        N = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr); //정렬
 
-    public static Queue<Vertex> queue = new LinkedList<>();
+        M = Integer.parseInt(br.readLine());
+        res = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-    public static int[][] DIRECTIONS = {{-1,0},{0,1},{1,0},{0,-1} };
-
-    public static void main(String[] args) throws IOException {
-
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
-
-        graph = new int[n][m];
-        bfsVisited = new boolean[n][m];
-        minDepth = new int[n][m];
-        init();
-        bfs();
-        System.out.println(minDepth[n-1][m-1]);
-
-    }
-
-    public static void bfs(){
-        minDepth[0][0] = 1;
-        queue.offer(new Vertex(0,0));
-        while(!queue.isEmpty()){
-            Vertex v = queue.poll();
-            if(bfsVisited[v.row][v.col]) continue;
-            bfsVisited[v.row][v.col] = true;
-            for(Vertex adj : getAdjacent(v)){
-                if(!bfsVisited[adj.row][adj.col]){
-                    minDepth[adj.row][adj.col] = minDepth[v.row][v.col] +1;
-                    queue.offer(adj);
-                }
-            }
+        for(int i=0; i<M; i++) {
+            if(binarySearch(0, N-1, res[i]) != null)
+                sb.append('1').append('\n');
+            else
+                sb.append('0').append('\n');
         }
+        System.out.println(sb);
     }
 
-    public static List<Vertex> getAdjacent(Vertex v){
-        List<Vertex> arr = new ArrayList<>();
-        for(int[] dir : DIRECTIONS){
-            int newRow = v.row + dir[0];
-            int newCol = v.col + dir[1];
-            if(0<=newRow && newRow <=n-1 && 0<=newCol && newCol<=m-1 &&graph[newRow][newCol]==1){
-                arr.add(new Vertex(newRow, newCol));
-            }
+    public static Integer binarySearch(int low, int high, int k) {
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[mid] > k) high = mid-1;
+            else if(arr[mid] < k) low = mid +1 ;
+            else return k;
         }
-        return arr;
-
+        return null; // 못 찾았다는 의미
     }
-
-    public static void init() throws IOException{
-        for(int i=0; i<n; i++){
-            String[] s = br.readLine().split("");
-            for(int j=0; j<m; j++){
-                graph[i][j] = Integer.parseInt(s[j]);
-            }
-        }
-    }
-
-    static class Vertex{
-        int row;
-        int col;
-
-        public Vertex(int row, int col){
-            this.row = row;
-            this.col = col;
-        }
-    }
-
 
 }
