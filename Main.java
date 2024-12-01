42ebd855f52c27c6b4e4a2c02c3d983fb448c483
jay <rlaakswo0687@gmail.com>
2024-12-01 13:29:41 +0900

    Boj 2512

--- Main.java
+++ Main.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
-
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int[] arr;
     public static int n;
     public static int m;
+    public static int[] arr;
     public static int max = 0;
+    public static int answer =0;
     public static void main(String[] args) throws IOException {
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]); m = Integer.parseInt(s[1]);
-        String[] inputs = br.readLine().split(" ");
+        n = Integer.parseInt(br.readLine());
         arr = new int[n];
+        String[] s = br.readLine().split(" ");
         for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(inputs[i]);
+            arr[i] = Integer.parseInt(s[i]);
             max = Math.max(max, arr[i]);
         }
+        m = Integer.parseInt(br.readLine());
 
         int low = 1;
         int high = max;
-        int h = 0;
 
-        while(low<=high){
-            int mid = low + (high - low)/ 2; // h
-            long treeSize = slice(mid);
-            if(treeSize >= m) {
+        while(low<= high){
+            int mid = low + (high -low) / 2;
+            if(isAssignable(mid)) {
                 low = mid +1;
-                h = mid;
+                answer = mid;
             }
-            else high = mid -1;
+            else high = mid-1;
         }
-        System.out.println(h);
+        System.out.println(answer);
     }
 
-    public static long slice(int h){
-        long sum = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i] >= h) sum += arr[i] - h;
-        }
-        return sum;
+    public static boolean isAssignable(int money){
+        int sum = 0;
+        for(int i=0; i<n; i++)
+            sum += Math.min(money, arr[i]);
+        return m >= sum;
     }
+
+
 }
