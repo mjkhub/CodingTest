63a7169d245eaca932e9ef13eda6ea6d7062a02f
jay <rlaakswo0687@gmail.com>
2024-12-01 14:29:13 +0900

    Boj 2343

--- Main.java
+++ Main.java
@@ -7,38 +7,52 @@ public class Main {
     public static int n;
     public static int m;
     public static int[] arr;
-    public static int max = 0;
-    public static int answer =0;
+
+    public static int max =0;
+    public static int totalSum = 0;
+    public static int answer = 0;
+
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
         String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+        arr = new int[n];
+        String[] input = br.readLine().split(" ");
         for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(s[i]);
+            arr[i] = Integer.parseInt(input[i]);
             max = Math.max(max, arr[i]);
+            totalSum += arr[i];
         }
-        m = Integer.parseInt(br.readLine());
 
-        int low = 1;
-        int high = max;
+        int low = max;
+        int high = totalSum;
 
-        while(low<= high){
-            int mid = low + (high -low) / 2;
-            if(isAssignable(mid)) {
-                low = mid +1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(canDivide(mid)) {
+                high = mid -1;
                 answer = mid;
+            }else{
+                low = mid +1;
             }
-            else high = mid-1;
         }
         System.out.println(answer);
     }
 
-    public static boolean isAssignable(int money){
-        int sum = 0;
-        for(int i=0; i<n; i++)
-            sum += Math.min(money, arr[i]);
-        return m >= sum;
-    }
+    public static boolean canDivide(int size){
+        int count = 1;
+        int sum =0;
+        for(int i=0; i<n; i++){
+            if( sum + arr[i] > size){
+                count++;
+                sum = arr[i];
+                if(count > m) return false;
+            }else{
+                sum += arr[i];
+            }
 
+        }
+        return true;
+    }
 
 }
