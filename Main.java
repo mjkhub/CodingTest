80c7824b0612e2a536271fc8a9beff20d0629663
jay <rlaakswo0687@gmail.com>
2024-12-16 21:08:18 +0900

    Boj 2167

--- Main.java
+++ Main.java
@@ -1,58 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static int n;
     public static int m;
-    public static int[] arr;
-
-    public static int max =0;
-    public static int totalSum = 0;
-    public static int answer = 0;
+    public static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         String[] s = br.readLine().split(" ");
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
-        arr = new int[n];
-        String[] input = br.readLine().split(" ");
+        arr = new int[n][m];
+
         for(int i=0; i<n; i++){
-            arr[i] = Integer.parseInt(input[i]);
-            max = Math.max(max, arr[i]);
-            totalSum += arr[i];
-        }
-
-        int low = max;
-        int high = totalSum;
-
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(canDivide(mid)) {
-                high = mid -1;
-                answer = mid;
-            }else{
-                low = mid +1;
+            String[] values = br.readLine().split(" ");
+            for(int j=0; j<m; j++){
+                arr[i][j] = Integer.parseInt(values[j]);
             }
         }
-        System.out.println(answer);
+
+        int k = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<k; i++){
+            int[] indexList = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int sum = getSum(indexList);
+            System.out.println(sum);
+        }
+
     }
 
-    public static boolean canDivide(int size){
-        int count = 1;
+    public static int getSum(int[] indexList){
         int sum =0;
-        for(int i=0; i<n; i++){
-            if( sum + arr[i] > size){
-                count++;
-                sum = arr[i];
-                if(count > m) return false;
-            }else{
-                sum += arr[i];
+        for(int i=indexList[0]-1; i<=indexList[2]-1; i++){
+            for(int j=indexList[1]-1; j<=indexList[3]-1; j++){
+                sum +=arr[i][j];
             }
-
         }
-        return true;
+        return sum;
     }
 
 }
---
+++ TimeComplexityComparison.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class TimeComplexityComparison {
+    public static void main(String[] args) {
+        final int DATA_SIZE = 100_000;
+        ArrayList<Integer> data = new ArrayList<>(DATA_SIZE);
+        Random random = new Random();
+
+        // 1. 데이터 생성 및 정렬
+        for (int i = 0; i < DATA_SIZE; i++) {
+            data.add(random.nextInt(DATA_SIZE * 10));
+        }
+        Collections.sort(data); // 이진 탐색을 위해 정렬
+
+        int target = data.get(random.nextInt(DATA_SIZE)); // 찾을 데이터 중 하나를 랜덤 선택
+
+        // 2. O(log n) - 이진 탐색
+        long startTime = System.nanoTime();
+        int indexLogN = binarySearch(data, target);
+        long endTime = System.nanoTime();
+        System.out.println("O(log n) - Binary Search Index: " + indexLogN);
+        System.out.println("O(log n) - 실행 시간: " + (endTime - startTime) / 1_000_000.0 + " ms");
+
+        // 3. O(n) - 선형 탐색
+        startTime = System.nanoTime();
+        int indexN = linearSearch(data, target);
+        endTime = System.nanoTime();
+        System.out.println("O(n) - Linear Search Index: " + indexN);
+        System.out.println("O(n) - 실행 시간: " + (endTime - startTime) / 1_000_000.0 + " ms");
+    }
+
+    // 이진 탐색 O(log n)
+    public static int binarySearch(ArrayList<Integer> data, int target) {
+        int left = 0, right = data.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (data.get(mid) == target) return mid;
+            if (data.get(mid) < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1; // 찾지 못했을 때
+    }
+
+    // 선형 탐색 O(n)
+    public static int linearSearch(ArrayList<Integer> data, int target) {
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i) == target) return i;
+        }
+        return -1; // 찾지 못했을 때
+    }
+}
