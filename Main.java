d48521fc82f0e04e74746a1fe30063873fc65cab
jay <rlaakswo0687@gmail.com>
2024-12-24 13:05:00 +0900

    Boj 21318

--- Main.java
+++ Main.java
@@ -7,40 +7,47 @@ public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int n, q;
-
-    public static List<Integer> arr = new ArrayList<>();
+    public static int n;
+    public static int[] arr;
+    public static boolean[] mistake;
     public static int[] sum;
+
 
     public static void main(String[] args) throws IOException { // ok 근데 이거 코딩 스타일이 너무 안좋은 것 같음
 
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]); q = Integer.parseInt(s[1]);
-        sum = new int[n+1];
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n+2];
+        mistake = new boolean[n+2];
+        sum = new int[n+2];
+        arr[n+1] = Integer.MAX_VALUE;
 
-        String[] s1 = br.readLine().split(" ");
-        for(int i=0; i<n; i++)
-            arr.add(Integer.parseInt(s1[i]));
+        String[] input = br.readLine().split(" ");
+        for(int i=n; i>=1; i--){
+            int level = Integer.parseInt(input[i-1]);
+            if(level > arr[i+1]){
+                mistake[i] = true;
+            }
+            arr[i] = level;
+        }
+        for(int i=1; i<=n; i++){
+            sum[i] = sum[i-1] + (mistake[i]?1:0);
+        }
 
-        Collections.sort(arr);
-        for(int i=1; i<=n; i++)
-            sum[i] = sum[i-1] + arr.get(i-1);
-
+        int q = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-
         for(int i=0; i<q; i++){
-            String[] s2 = br.readLine().split(" ");
-            int start = Integer.parseInt(s2[0]);
-            int end = Integer.parseInt(s2[1]);
-            int result = getSum(start,end);
-            sb.append(result).append('\n');
+            String[] s = br.readLine().split(" ");
+            int start = Integer.parseInt(s[0]);
+            int end = Integer.parseInt(s[1]);
+            sb.append(getSum(start, end)).append('\n');
         }
         System.out.println(sb);
-
     }
 
     public static int getSum(int start, int end){
-        return sum[end] - sum[start-1];
+        int answer = sum[end] - sum[start-1];
+        if(mistake[end]) answer -=1;
+        return answer;
     }
 
 }
