3ef522baf6e4d82c511198fd53f407e5ac1a0827
jay <rlaakswo0687@gmail.com>
2024-12-28 02:29:05 +0900

    BOJ 1158

--- Main.java
+++ Main.java
@@ -10,35 +10,25 @@ public class Main {
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        List<Balloon> list = new LinkedList<>();
-
         String[] s = br.readLine().split(" ");
-        for(int i=0; i<n; i++)
-            list.add(new Balloon(i+1, Integer.parseInt(s[i])));
+        int n = Integer.parseInt(s[0]);
+        int k = Integer.parseInt(s[1]);
 
-        int index = 0;
-        while(!list.isEmpty()){
-            Balloon b = list.remove(index);
-            sb.append(b.number).append(' ');
-            if(list.isEmpty()) break; // last case
+        List<Integer> arr = new LinkedList<>();
+        for(int i=1; i<=n; i++) arr.add(i);
 
-            int move = b.weight; // 이 핵심 로직을 생각 못함
-            if(move > 0 ) index = (index + b.weight - 1) % list.size();
-            else index = ( index + b.weight) % list.size();
-            if(index < 0) index = list.size() + index;
+        sb.append("<");
+        int index = k-1;
+        while(!arr.isEmpty()){
+            Integer remove = arr.remove(index);
+            sb.append(remove);
+            if(arr.isEmpty())
+                break;
+            sb.append(", ");
+            index = (index-1 +k) % arr.size();
         }
+        sb.append(">");
         System.out.println(sb);
-    }
-
-    static class Balloon{
-        int number; // 맨 처음에 있던 순서
-        int weight; // 인덱스
-
-        public Balloon(int number, int weight){
-            this.number = number;
-            this.weight = weight;
-        }
     }
 
 
