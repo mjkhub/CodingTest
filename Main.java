e6027eff7fd4099e4ba618e0f2a0eb920d1d36b0
jay <rlaakswo0687@gmail.com>
2025-01-03 16:17:37 +0900

    BOJ 14721

--- Main.java
+++ Main.java
@@ -6,45 +6,52 @@ import java.util.*;
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int n, m;
-    public static char[][] map;
-    public static String[][] boards = {{"WBWBWBWB","BWBWBWBW", "WBWBWBWB","BWBWBWBW", "WBWBWBWB","BWBWBWBW", "WBWBWBWB","BWBWBWBW"},
-            {"BWBWBWBW","WBWBWBWB", "BWBWBWBW","WBWBWBWB", "BWBWBWBW","WBWBWBWB", "BWBWBWBW","WBWBWBWB"}}; // 0: white 1: black
+
+    public static int n;
+
+    public static List<Tuple> arr = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]); m = Integer.parseInt(s[1]);
-        map = new char[n][m];
+        n = Integer.parseInt(br.readLine());
         for(int i=0; i<n; i++){
-            String input = br.readLine();
-            for(int j=0; j<m; j++)
-                map[i][j] = input.charAt(j);
+            String[] s = br.readLine().split(" ");
+            arr.add(new Tuple(Integer.parseInt(s[0]),Integer.parseInt(s[1])));
         }
 
-        int min = Integer.MAX_VALUE;
-
-        int maxRow = n-8;
-        int maxCol = m-8;
-
-        for(int i=0; i<=maxRow; i++){
-            for(int j=0; j<=maxCol; j++){
-                int white = countDiff(i,j,0);
-                int black = countDiff(i,j,1);
-                min = Math.min(Math.min(white, black),min);
+        Tuple answer = new Tuple(0,0);
+        long min = Long.MAX_VALUE;
+        for(int a=1; a<=100; a++){ // 브루트 포스
+            for(int b=1; b<=100; b++){
+                long rss = calculateRss(a,b);
+                if(rss < min){
+                    min = rss;
+                    answer.x = a;
+                    answer.y = b;
+                }
             }
         }
-        System.out.println(min);
-
+        System.out.println(answer.x + " " + answer.y);
     }
 
-    public static int countDiff(int sR, int sC, int boardIndex){
-        String[] board = boards[boardIndex];
-        int count = 0;
-        for(int i=sR; i<sR+8; i++){
-            for(int j=sC; j<sC+8; j++){
-                if(map[i][j] != board[i-sR].charAt(j-sC))count++;
-            }
+    public static long calculateRss(int a, int b){
+        long rss = 0;
+        for(int i=0; i<n; i++){
+            Tuple tuple = arr.get(i);
+            int xi = tuple.x;
+            int yi = tuple.y;
+            int fx = a * xi + b;
+            rss += (long)(yi-fx) * (yi-fx); // overflow could occur
         }
-        return count;
+        return rss;
+    }
+
+    static class Tuple{
+        int x;
+        int y;
+
+        public Tuple(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 
 
