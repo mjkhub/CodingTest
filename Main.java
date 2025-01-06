e20b562409f15590fa95449aee89218d8cec4929
jay <rlaakswo0687@gmail.com>
2025-01-06 15:04:30 +0900

    BOJ 1347

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -6,67 +8,90 @@ import java.util.*;
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public static int[] cards;
-    public static List<List<Integer>> answer;
     public static int n;
-    public static int[] p, s;
+    public static String content;
+    public static int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}}; // 0 ~ 3
+    public static int dir = 1;
+
+    public static int row=0, col=0; // 초기 좌표
+    public static List<Position> path = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
-        answer = new ArrayList<>();
-        for(int i=0; i<3; i++) answer.add(new ArrayList<>());
+        content = br.readLine();
 
-        cards = new int[n];
-        for(int i=0; i<n; i++) cards[i] = i;
-
-        p = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        s = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
-        makeAnswer();
-        if(isAnswer()){
-            System.out.println(0);
-            return;
+        path.add(new Position(row,col));
+        for(int i=0; i<n; i++){
+            char c = content.charAt(i);
+            if(c == 'F') move();
+            else changeDir(c);
         }
 
-        int count =0;
-        while(true){
-            swap();
-            count++;
-            if(isAnswer())
-                break;
-            if(count == Integer.MAX_VALUE / 10000 ){ // 이게 맞나 싶긴한데 . . .
-                System.out.println(-1);
-                return;
+        int min = 0;
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for(int i=1; i<path.size(); i++){
+            Position p1 = path.get(min);
+            Position temp = path.get(i);
+            if(p1.row >= temp.row && p1.col >= temp.col)
+                min = i;
+        }
+
+        for(int i=0; i<path.size(); i++){
+            if( min == i ) continue;
+            Position m= path.get(min);
+            Position p = path.get(i);
+            int rowAbs = Math.abs(p.row - m.row);
+            int colAbs = Math.abs(p.col - m.col);
+            if( rowAbs > maxRow) maxRow = rowAbs;
+            if( colAbs > maxCol) maxCol = colAbs;
+        }
+        maxCol +=1;
+        maxRow +=1;
+        boolean[][] answer = new boolean[maxRow][maxCol];
+        while(!path.isEmpty()){
+            Position p = path.remove(0);
+            answer[p.row+ maxRow-1][p.col] = true;
+        }
+
+        for(int i=0; i<maxRow; i++){
+            for(int j=0; j<maxCol; j++){
+                System.out.print(answer[i][j]?'.':'#');
             }
+            System.out.println();
         }
-        System.out.println(count);
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
-    public static void makeAnswer(){
-        for(int i=0; i<n; i++){
-            List<Integer> playerCards = answer.get(p[i]);
-            playerCards.add(i);
-        }
+    public static void move(){
+        row = row + directions[dir][0];
+        col = col + directions[dir][1];
+        path.add(new Position(row,col));
     }
 
-    public static void swap(){
-        int[] swappedCards = new int[n];
-
-        for(int i=0; i<n; i++){
-            swappedCards[s[i]] = cards[i];
-        }
-        cards = swappedCards;
+    public static void changeDir(char c){
+        if(c == 'R') dir = (dir + 1) % 4;
+        else dir = (dir + 3) % 4;
     }
 
-
-    public static boolean isAnswer(){
-        for(int i=0; i<n; i++){
-            int playerNumber = i % 3;
-            if(!answer.get(playerNumber).contains(cards[i])) return false;
+    static class Position{
+        int row;
+        int col;
+        public Position(int row, int col){
+            this.row = row;
+            this.col = col;
         }
-        return true;
     }
-
 
 }
