a8458bebf3a4cfa96d7767d1444955f516d43ccf
jay <rlaakswo0687@gmail.com>
2025-01-13 12:01:43 +0900

    BOJ 11721

--- Main.java
+++ Main.java
@@ -7,25 +7,19 @@ public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-
-
         String s = br.readLine();
 
         int startIndex = 0;
         int endIndex = 10;
-
         int totalCount = (int)Math.ceil((double)s.length() / 10);
+
         for(int i=0; i<totalCount; i++){
             if(endIndex > s.length())
                 endIndex = s.length();
-
             System.out.println(s.substring(startIndex,endIndex));
             startIndex += 10;
             endIndex +=10;
-
         }
-
-
     }
 
 }
