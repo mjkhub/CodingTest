f39cace1ee6a8258adcbd40ea1b45b9373e7769f
jay <rlaakswo0687@gmail.com>
2025-01-17 10:30:50 +0900

    BOJ 17952

--- Main.java
+++ Main.java
@@ -1,48 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int[] heap;
-    public static int totalNode;
-
-    public static String[] input;
-
     public static void main(String[] args) throws IOException {
-        int k = Integer.parseInt(br.readLine());
-        input = br.readLine().split(" ");
 
-        totalNode = (int)Math.pow(2,k)-1;
-        heap = new int[totalNode+1];
+        Stack<Task> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
 
-        fillNode(0, totalNode-1, 1);
-
-
-        StringBuilder sb = new StringBuilder();
-        int index = 1;
-        for(int i=1; i<=k; i++){ // 각 depth
-            int count = (int)Math.pow(2,i) / 2 ;
-            for(int j=0; j<count; j++){
-                sb.append(heap[index++]).append(' ');
+        while(n-->0){
+            String[] s = br.readLine().split(" ");
+            int yes = Integer.parseInt(s[0]);
+            if(yes == 0){
+                if(!stack.isEmpty()){ //내가 부족한 부분
+                    Task t = stack.peek();
+                    t.time -=1;
+                    if(t.time == 0){
+                        count += t.score;
+                        stack.pop();
+                    }
+                }
+            }else{
+                int score = Integer.parseInt(s[1]);
+                int t = Integer.parseInt(s[2]);
+                if(t == 1)
+                    count += score;
+                else
+                    stack.push(new Task(score,t-1));
             }
-            sb.append('\n');
         }
-        System.out.println(sb);
-
-
-    }
-
-    public static void fillNode(int start, int end, int i){
-        int mid = (start + end )/ 2;
-        heap[i] = Integer.parseInt(input[mid]);
-
-        if(start < mid) fillNode(start, mid-1, i*2);
-        if(end > mid) fillNode(mid+1, end, i*2+1);
-        // base case 는 start = mid
+        System.out.println(count);
 
     }
 
+    static class Task{
+        int score;
+        int time;
+
+        public Task(int score, int time) {
+            this.score = score;
+            this.time = time;
+        }
+
+    }
 }
