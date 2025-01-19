51c3343a5e5bb1397e8bbbf04e79f1752b9b1ecf
jay <rlaakswo0687@gmail.com>
2025-01-19 18:44:17 +0900

    BOJ 5212

--- Main.java
+++ Main.java
@@ -1,96 +1,94 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.*;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static Student[] arr;
-    public static int n;
+    public static int r, c;
+
+    public static int[][] map;
+
+
     public static void main(String[] args) throws IOException {
 
-        n = Integer.parseInt(br.readLine());
-        arr = new Student[n];
-        int k = Integer.parseInt(br.readLine());
-        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String[] s = br.readLine().split(" ");
+        r =  Integer.parseInt(s[0]);
+        c =  Integer.parseInt(s[1]);
+        map = new int[r][c];
 
-        for(int i=0; i<k; i++){
-            int number = numbers[i];
-            int index = contains(number);
-            if(index != -1) // 이미 존재
-                arr[index].count +=1;
-            else{ //새로운 위치를 할당 받고 넣어야함
-                index = getMinIndex();
-                arr[index] = new Student(number);
-            }
-            updateTime(); // 시간 Up
-        }
-
-
-        List<Integer> answer = new ArrayList<>();
-
-        for(int i=0; i<n; i++){
-            if(arr[i] != null)
-                answer.add(arr[i].number);
-        }
-
-        Collections.sort(answer);
-        for(int i=0; i<answer.size(); i++)
-            System.out.print(answer.get(i)+" ");
-
-    }
-
-    public static int contains(int number){
-        for(int i=0; i<n; i++){
-            if(arr[i]!=null && arr[i].number==number)
-                return i;
-        }
-        return -1;
-    }
-
-    public static int getMinIndex(){
-        int minIndex = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i] == null) return i;
-            else{
-                if(arr[minIndex].count > arr[i].count )
-                    minIndex = i;
-                else if(arr[minIndex].count == arr[i].count
-                && arr[minIndex].time < arr[i].time)
-                    minIndex = i;
+        for(int i=0; i<r; i++){
+            String line = br.readLine();
+            for(int j=0; j<c; j++){
+                char c = line.charAt(j);
+                if( c == '.') map[i][j] = 0;
+                else map[i][j] = 1;
             }
         }
-        return minIndex;
-    }
-    public static void updateTime(){
-        for(int i=0; i<n; i++){
-            if(arr[i]!= null){
-                arr[i].updateTime();
+
+        after50Year();
+
+        int minRow = Integer.MAX_VALUE, maxRow = Integer.MIN_VALUE;
+        int minCol = Integer.MAX_VALUE, maxCol = Integer.MIN_VALUE;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (map[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = minRow; i <= maxRow; i++) {
+            for (int j = minCol; j <= maxCol; j++) {
+                sb.append(map[i][j] == 1 ? 'X' : '.');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+
     }
 
-    static class Student{
-        int number;
-        int time;
-        int count;
 
-        public Student (int number){
-            this.number = number;
-            this.time = 0;
-            this.count = 1;
+
+
+    public static void after50Year(){
+        int[][] newMap = new int[r][c];
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                int count = getSeaCount(i,j);
+                if( count >=3) newMap[i][j] = 0; //바다
+                else newMap[i][j] = map[i][j]; // 그대로
+            }
         }
-
-        public void updateTime(){
-            time++;
-        }
-
-        public int getNumber(){
-            return number;
-        }
-
+        map = newMap;
     }
+
+    public static int getSeaCount(int i, int j){
+
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        int seaCount = 0;
+        for(int[] dir : directions){
+            int row = i + dir[0];
+            int col = j + dir[1];
+
+            if(!(0<=row && row<r && 0<=col && col<c)) {
+                seaCount++;
+            }else{
+                if(map[row][col]==0)
+                    seaCount++;
+            }
+        }
+        return seaCount;
+    }
+
+
 
 }
