0f41c92af8b4cc916214fe05d3e7396423330a68
jay <rlaakswo0687@gmail.com>
2025-01-22 11:51:17 +0900

    BOJ 12873

--- Main.java
+++ Main.java
@@ -1,54 +1,30 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
 
+    public static int n;
+    public static List<Integer> player = new ArrayList<>();
+
+    public static int beforeIndex = 0;
     public static void main(String[] args) throws IOException {
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        for(int i=1; i<=n; i++) player.add(i);
 
-        while(n-->0){
-            String s = br.readLine();
-            System.out.println(getSize(s));
+        for(int i=0; i<n-1; i++){
+            long count = (long)Math.pow(i+1, 3);
+            int removeIndex = (int)((beforeIndex + count -1) % player.size());
+            player.remove(removeIndex);
+            beforeIndex = removeIndex;
         }
+        System.out.println(player.get(0));
 
-    }
-
-    public static int getSize(String s){
-
-        int dir = 0;
-        int x=0; int y = 0;
-        int minX = 0;
-        int maxX = 0;
-        int minY = 0;
-        int maxY = 0;
-
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(c == 'F'){
-                x = x + directions[dir][0];
-                y = y + directions[dir][1];
-            }else if(c == 'B'){
-                x = x - directions[dir][0];
-                y = y - directions[dir][1];
-            }else if(c == 'L'){
-                dir  = (dir+3)%4;
-            }else{ // R
-                dir  = (dir+1)%4;
-            }
-            minX = Math.min(x,minX);
-            maxX = Math.max(x,maxX);
-            minY = Math.min(y,minY);
-            maxY = Math.max(y,maxY);
-        }
-
-
-
-        return (maxY-minY) * (maxX - minX);
     }
 
 
