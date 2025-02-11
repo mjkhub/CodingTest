ebf536ef528d65e34b6587d3e5b42576af181fa1
jay <rlaakswo0687@gmail.com>
2025-02-11 13:05:59 +0900

    BOJ 2110

--- Main.java
+++ Main.java
@@ -1,65 +1,48 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int n;
-
-    public static String first;
-
-    public static int[] countArr;
-    public static int answer = 0;
-
+    public static int n, m;
+    public static int[] arr;
     public static void main(String[] args) throws IOException {
 
-        n = Integer.parseInt(br.readLine());
-        first = br.readLine();
-        countArr = getCountArr(first);
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]); m = Integer.parseInt(s[1]);
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
 
-        while(n-->1){
-            String input = br.readLine();
-            if(isSimilar(input))
-                answer++;
+        int low = 1;
+        int high =  arr[n-1] - arr[0];
+        int answer = 0;
+
+        while(low <= high){
+            int mid = (high + low) / 2;
+            if(isAvailable(mid)){
+                answer = mid;
+                low = mid + 1;
+            }else
+                high = mid-1;
         }
         System.out.println(answer);
 
     }
 
-    public static boolean isSimilar(String input){
-        int[] inputArr = getCountArr(input);
-        int[] subArr = new int[26];
+    public static boolean isAvailable(int size) {
+        int count = 1;
+        int prev = arr[0];
 
-        for(int i=0; i<26; i++)
-            subArr[i] = Math.abs(inputArr[i]-countArr[i]);
-
-        int oneCount = 0;
-
-        for(int i=0; i<26; i++){
-            if(subArr[i] > 1) return false;
-            else if(subArr[i] == 1)
-                oneCount +=1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - prev >= size) {
+                count++;
+                prev = arr[i];
+            }
         }
 
-        int lengthAbs = Math.abs(input.length() - first.length());
-        if(oneCount == 0 ) return true;
-        else if(oneCount == 1 && lengthAbs == 1 ) return true;
-        else if(oneCount == 2 && lengthAbs == 0) return true;
-        else return false;
-
+        return count >= m;
     }
-
-    public static int[] getCountArr(String input){
-        int[] countArr = new int[26];
-        for(int i=0; i<input.length(); i++){
-            char c = input.charAt(i);
-            countArr[c-'A'] += 1;
-        }
-        return countArr;
-    }
-
-
 }
