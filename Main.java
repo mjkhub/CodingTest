4282bdfe96c1f3f38369c930f2fe3574f8ec8c4b
jay <rlaakswo0687@gmail.com>
2025-02-11 15:23:37 +0900

    BOJ 2117

--- Main.java
+++ Main.java
@@ -1,48 +1,29 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public static int n, m;
-    public static int[] arr;
     public static void main(String[] args) throws IOException {
 
-        String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]); m = Integer.parseInt(s[1]);
-        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++)
+            arr[i] = Integer.parseInt(br.readLine());
         Arrays.sort(arr);
 
-        int low = 1;
-        int high =  arr[n-1] - arr[0];
-        int answer = 0;
-
-        while(low <= high){
-            int mid = (high + low) / 2;
-            if(isAvailable(mid)){
-                answer = mid;
-                low = mid + 1;
-            }else
-                high = mid-1;
+        int answer = 0 ;
+        int count =0;
+        for(int i=n-1; i>=0; i--){
+            count++;
+            answer = Math.max(count*arr[i], answer);
         }
         System.out.println(answer);
 
     }
 
-    public static boolean isAvailable(int size) {
-        int count = 1;
-        int prev = arr[0];
-
-        for (int i = 1; i < n; i++) {
-            if (arr[i] - prev >= size) {
-                count++;
-                prev = arr[i];
-            }
-        }
-
-        return count >= m;
-    }
 }
