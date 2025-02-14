20cf0aad5236990dfae8c9b0fa1c05f602f76e34
jay <rlaakswo0687@gmail.com>
2025-02-14 15:26:34 +0900

    BOJ 7576

--- Main.java
+++ Main.java
@@ -1,61 +1,85 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
+    public static int row, col;
     public static int[][] arr;
     public static boolean[][] visit;
-
-    public static int w, h;
-
-    public static int[][] directions = {{-1,-1}, {-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+    public static int[][] directions = {{-1,0},{0,1},{1,0},{0,-1}};
+    public static Queue<Tomato> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        row = Integer.parseInt(s[1]); col = Integer.parseInt(s[0]);
 
+        arr = new int[row][col];
+        visit = new boolean[row][col];
 
-        while(true){
-            String[] input = br.readLine().split(" ");
-            w = Integer.parseInt(input[0]);
-            h = Integer.parseInt(input[1]);
-            if(w==0 && h==0) break;
-
-            arr = new int[h][w];
-            visit = new boolean[h][w];
-            for(int i=0; i<h; i++){
-                String[] s = br.readLine().split(" ");
-                for(int j=0; j<w; j++){
-                    arr[i][j] = Integer.parseInt(s[j]);
+        int tomatoCount = 0;
+        boolean all = true;
+        for(int i=0; i<row; i++){
+            String[] line = br.readLine().split(" ");
+            for(int j=0; j<col; j++){
+                arr[i][j] = Integer.parseInt(line[j]);
+                if(arr[i][j] == 0){
+                    all = false;
+                    tomatoCount++;
                 }
-            }
+                else if(arr[i][j] == 1)
+                    queue.offer(new Tomato(new int[]{i,j},0));
 
-            int count =0;
-            for(int i=0; i<h; i++){
-                for(int j=0; j<w; j++){
-                    if(!visit[i][j] && arr[i][j] == 1){
-                        count++;
-                        dfs(i,j);
-                    }
-                }
-            }
-            System.out.println(count);
-
-        }
-    }
-
-    public static void dfs(int row, int col){
-        visit[row][col] = true;
-
-        for(int[] dir: directions){
-            int nr = row + dir[0];
-            int nc = col + dir[1];
-            if(0<=nr && nr <h && 0<=nc && nc < w && !visit[nr][nc] && arr[nr][nc] == 1){
-                dfs(nr,nc);
             }
         }
+        if(all){
+            System.out.println(0);
+            return;
+        }
+
+        int day= 0 ;
+        while(!queue.isEmpty()){
+            Tomato tm = queue.poll();
+            visit[tm.pos[0]][tm.pos[1]] = true;
+            for(int[] pos : getAdjacentUnMatureTomato(tm.pos)){
+                if(!visit[pos[0]][pos[1]]){
+                    Tomato temp = new Tomato(pos, tm.depth +1);
+                    tomatoCount--;
+                    day = Math.max(day, temp.depth);
+                    arr[pos[0]][pos[1]] = 1;
+                    queue.offer(temp);
+                }
+            }
+        }
+        System.out.println( tomatoCount == 0 ? day : -1);
+
     }
 
+
+    public static List<int[]> getAdjacentUnMatureTomato(int[] pos){
+        List<int[]> adjacent = new ArrayList<>();
+        for(int[] dir : directions){
+            int nr = pos[0]+ dir[0];
+            int nc = pos[1] + dir[1];
+            if(0<=nr && nr< row && 0<= nc && nc < col
+                    && !visit[nr][nc] && arr[nr][nc] == 0){
+                adjacent.add(new int[]{nr, nc});
+            }
+        }
+        return adjacent;
+    }
+
+    static class Tomato{
+        int[] pos;
+        int depth;
+
+        public Tomato(int[] pos, int depth){
+            this.pos = pos;
+            this.depth = depth;
+        }
+    }
 
 
 }
